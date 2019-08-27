ea00610b40712457be20994c1ea085c45d49a439
atCordain <pctkar@gmail.com>
2019-08-27 10:56:32 +0200

    Add project files.

---
+++ Labb4/Map.cs
@@ -0,0 +1,93 @@
+﻿using System;
+
+namespace Labb4
+{
+
+
+    class Map
+    {
+        int[][] intMap;
+        int playerXPosition, playerYPosition;
+
+        //TODO ändra till ENUM
+        enum Room { Empty, Wall, Monster, Key, Exit, Door, Player};
+        string[] mapCell = new string[] { ".", "#", "M", "N", "U", "D", "P" };
+
+        public Map (int[][] intMap)
+        {
+            this.IntMap = intMap;
+            SetupBoard();
+        }
+        
+        public void Print()
+        {
+            for (int i = 0; i < IntMap.Length; i++)
+            {
+                for (int j = 0; j < IntMap[i].GetLength(0); j++)
+                {
+                    Console.Write(mapCell[IntMap[i][j]]);
+                    Console.Write(" ");
+                }
+                Console.WriteLine();
+            }  
+        }
+
+        private void SetupBoard()
+        {
+            for (int i = 0; i < IntMap.Length; i++)
+            {
+                for (int j = 0; j < IntMap[i].GetLength(0); j++)
+                {
+                    if (IntMap[i][j] == (int)Room.Player)
+                    {
+                        playerYPosition = i;
+                        playerXPosition = j;
+                    }
+                }
+            }
+            
+        }
+        public int[][] IntMap { get => intMap; set => intMap = value; }
+
+        public void  MoveInDirection (Program.Direction dir)
+        {
+            switch (dir)
+            {
+                case Program.Direction.Up:
+                    IntMap[playerYPosition][playerXPosition] = (int)Room.Empty;
+                    playerYPosition -= 1;
+                    IntMap[playerYPosition][playerXPosition] = (int)Room.Player;
+                    break;
+                case Program.Direction.Down:
+
+                    break;
+                case Program.Direction.Left:
+                    break;
+                case Program.Direction.Right:
+                    break;
+            }
+            
+        }
+
+        /*
+        public Room GetRoomInDirection(Program.Direction dir)
+        {
+            Room room = new Room;
+            switch (dir)
+            {
+                case Program.Direction.Up:
+                    return room intMap[playerYPosition-1][playerXPosition];
+                    break;
+                case Program.Direction.Down:
+                    break;
+                case Program.Direction.Left:
+                    break;
+                case Program.Direction.Right:
+                    break;
+            }
+
+            return room;
+        }
+        */
+    }
+}
---
+++ Labb4/Program.cs
@@ -0,0 +1,61 @@
+﻿using System;
+
+namespace Labb4
+{
+    class Program
+    {
+        public enum Direction { Up, Down, Left, Right }
+        static void Main(string[] args)
+        {
+            Map map = new Map(new int[][]
+            {
+                new int [] { 1, 1, 1, 1, 1, 1, 1, 1, 1, 1 },
+                new int [] { 1, 0, 0, 0, 0, 0, 0, 0, 0, 1 },
+                new int [] { 1, 0, 0, 0, 0, 0, 0, 0, 0, 1 },
+                new int [] { 1, 0, 0, 6, 0, 0, 0, 0, 0, 1 },
+                new int [] { 1, 0, 0, 0, 0, 0, 0, 0, 0, 1 },
+                new int [] { 1, 0, 0, 0, 3, 0, 0, 0, 0, 1 },
+                new int [] { 1, 0, 0, 0, 0, 0, 0, 0, 0, 1 },
+                new int [] { 1, 0, 0, 0, 0, 0, 0, 0, 0, 1 },
+                new int [] { 1, 0, 0, 0, 0, 0, 0, 0, 4, 1 },
+                new int [] { 1, 1, 1, 1, 1, 1, 1, 1, 1, 1 },
+            });
+
+            bool playing = true;
+
+
+            // Main loop
+
+            Console.WriteLine("Welcome to Dungeon Crawler, you will navigate through the game and try to reach the exit through collecting keys in different enviroments and facing different obsticles");
+            Console.WriteLine("Where would you like to go?");
+
+            while (playing)
+            {
+                map.Print();
+                Console.WriteLine("Navigate through the map with the W A S D keys");
+                var playerDirection = Console.ReadKey().Key;
+                if (playerDirection == ConsoleKey.W)
+                {
+                    map.MoveInDirection(Direction.Up);
+                }
+                else if (playerDirection == ConsoleKey.S)
+                {
+                    map.MoveInDirection(Direction.Down);
+                }
+                else if (playerDirection == ConsoleKey.D)
+                {
+                    map.MoveInDirection(Direction.Right);
+                }
+                else if (playerDirection == ConsoleKey.A)
+                {
+                    map.MoveInDirection(Direction.Left);
+                }
+                Console.Clear();
+            }
+            
+
+            // TODO: GÖR EN FÖR X OCH EN FÖR Y istället!!!
+            Console.ReadKey();
+        }
+    }
+}
