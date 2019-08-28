cb6123194f3cf36a281e4012f207bf3af465c86f
Petter Fagerlund <petter.fagerlund@gmail.com>
2019-08-28 11:11:47 +0200

    Added Classes Player & HighScore, and added method isKeyValid to Player.  Changed TryEnter in Rooms.cs to check if key was valid through Player.isKeyValid().

---
+++ Labb4/HighScore.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Labb4
+{
+    class HighScore
+    {
+        public static string name;
+        public static int turns;
+        public HighScore()
+        {
+            name = Player.name;
+            turns = Player.turns;
+        }
+    }
+}
---
+++ Labb4/Player.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Labb4
+{
+    class Player
+    {
+        public static string name;
+        public static int keys; 
+        public static int turns;
+        public int playerX { get; set; }
+        public int playerY { get; set; }
+        public int playerSign { get; set; }
+
+        public Player()
+        {
+
+        }
+
+        public static bool isKeyValid()
+        {
+            return true; // just nu 
+        }
+    }
+}
