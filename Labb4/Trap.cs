f02cfa28ba95bdb9f834930270298972fc17bda6
atCordain <pctkar@gmail.com>
2019-08-28 15:06:59 +0200

    Added Secret trap room functionality

--- Labb4/Trap.cs
+++ Labb4/Trap.cs
@@ -4,7 +4,27 @@ using System.Text;
 
 namespace Labb4
 {
-    class Trap
+    class Trap : SquareClass , IRoom
     {
+        internal int trapDamage = 5;
+        internal bool notVisisted = true;
+        public Trap()
+        {
+            roomSign = ".";
+        }
+
+        public override string RoomSign { get => roomSign; set => roomSign = value; }
+
+        public bool TryEnter(Player player)
+        {
+            if (notVisisted)
+            {
+                Console.WriteLine($"You entered a Secret Trap Room, and lost {trapDamage} turns");
+                player.Turns += trapDamage;
+                notVisisted = false;
+            }
+
+            return true;
+        }
     }
 }
