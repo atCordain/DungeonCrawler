5471af481377b38a9a77d51b6c8925e8507a1398
Petter Fagerlund <petter.fagerlund@gmail.com>
2019-08-28 10:28:43 +0200

    Added Room Class, Added Wall Class & both extends Square Class and IRoom

---
+++ Labb4/Room.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Labb4
+{
+    class Room : SquareClass, IRoom
+    {
+        public override string RoomSign()
+        {
+            throw new NotImplementedException();
+        }
+
+        public override void Square(string inRoomSign)
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool TryEnter()
+        {
+            if (true ){
+                return true;     
+            } else {
+                return false;
+            }
+            throw new NotImplementedException();
+
+        }
+    }
+}
